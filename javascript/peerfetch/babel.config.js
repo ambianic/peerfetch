34443a81797018303f14a5afdacc63df9f6ea3eb
ivelin <ivelin@ambianic.ai>
2021-10-07 21:43:27 +0000

    chore: initial commit [skip-ci]
    Signed-off-by: ivelin <ivelin@ambianic.ai>

---
+++ javascript/peerfetch/babel.config.js
@@ -0,0 +1,17 @@
+// babel.config.js
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    'istanbul'
+  ]
+}
